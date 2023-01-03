d9a403439118e3f1ab319d677f7f792bd8fdae3f
i.manzhela <illiamanzhela01@gmail.com>
2023-01-04 00:02:41 +0100

    basic pages added

---
+++ frontend/src/MainSection/MainSection.jsx
@@ -0,0 +1,8 @@
+import "./MainSection.css";
+import Calendar from "./Pages/Calendar/Calendar";
+
+export default function MainSection() {
+    return (<div className="main-section">
+        <Calendar />
+    </div>);
+}
